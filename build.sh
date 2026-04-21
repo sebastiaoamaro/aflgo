06bcac085e3f749a3534bbdaa90e81172d437c5b
Sebastião Amaro <sebastiao.amaro@tecnico.ulisboa.pt>
2026-04-21 11:21:52 +0100

    fixed build.sh

--- build.sh
+++ build.sh
@@ -49,7 +49,7 @@ mv      compiler-rt-11.0.0.src        compiler-rt
 
 sudo sed -i '/#include <string>/a #include <cstdint>' /home/vagrant/aflgo/instrument/llvm_tools/llvm/include/llvm/Support/Signals.h
 
-sudo sed -i '0,/#include /s//#&\n#include <cstdlib>/' /home/vagrant/aflgo/instrument/llvm_tools/compiler-rt/lib/dfsan/dfsan_custom.cpp
+#sudo sed -i '/#include <string>/a #include <cstdint>' /home/vagrant/aflgo/instrument/llvm_tools/compiler-rt/lib/dfsan/dfsan_custom.cpp
 
 
 mkdir -p build
